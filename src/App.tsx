b30ad31ce39cb4e9ccf72c7538b616a750e4a4ba
sueleesoossoo <isoo7510@gmail.com>
2025-11-21 13:22:33 +0900

    setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupText, InputGroupTextarea } from './components/ui/input-group'
+import { ArrowUpIcon, PlusIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './components/ui/dropdown-menu'
+import { Separator } from './components/ui/separator'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [value, setValue] = useState('')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h2 className="scroll-m-20 px-5 pb-2 text-3xl font-semibold tracking-tight first:mt-0">지금 무슨 생각을 하고 있어?</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <InputGroup>
+          <InputGroupTextarea placeholder="Ask, Search or Chat..." 
+            value={value}
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+              setValue(e.target.value)
+          }/>
+          <InputGroupAddon align="block-end">
+            <InputGroupButton
+              variant="outline"
+              className="rounded-full"
+              size="icon-xs"
+            >
+              <PlusIcon />
+            </InputGroupButton>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <InputGroupButton variant="ghost">Auto</InputGroupButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                align="start"
+                className="[--radius:0.95rem]"
+              >
+                <DropdownMenuItem>Auto</DropdownMenuItem>
+                <DropdownMenuItem>Agent</DropdownMenuItem>
+                <DropdownMenuItem>Manual</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <InputGroupText className="ml-auto">52% used</InputGroupText>
+            <Separator orientation="vertical" className="!h-4" />
+            <InputGroupButton
+              variant="default"
+              className="rounded-full"
+              size="icon-xs"
+              disabled
+            >
+              <ArrowUpIcon />
+              <span className="sr-only">Send</span>
+            </InputGroupButton>
+          </InputGroupAddon>
+        </InputGroup>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
