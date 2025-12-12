40c327099362db670dd478707d93039246606d57
sueleesoossoo <isoo7510@gmail.com>
2025-12-12 18:04:40 +0900

    feat: implement conditional rendering for chat views based on message availability

--- src/routes/f/$conversationId.tsx
+++ src/routes/f/$conversationId.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from '@tanstack/react-router'
 
 import { ChatDetailView } from '@/features/chat/views/ChatDetailView'
 import { ChatInputProvider } from '@/contexts/ChatInputProvider'
+import { useMessagesQuery } from '@/features/chat/hooks/queries/useMessagesQuery'
+import { NewChatView } from '@/features/chat/views/NewChatView'
 
 export const Route = createFileRoute('/f/$conversationId')({
   component: RouteComponent,
@@ -10,9 +12,16 @@ export const Route = createFileRoute('/f/$conversationId')({
 function RouteComponent() {
   const { conversationId } = Route.useParams()
 
+  const { data: messages } = useMessagesQuery(conversationId)
+  const hasChat = (messages?.length ?? 0) > 0
+
   return (
     <ChatInputProvider conversationId={conversationId}>
-      <ChatDetailView conversationId={conversationId} />
+      {hasChat ? (
+        <ChatDetailView conversationId={conversationId} />
+      ) : (
+        <NewChatView disabled={false} />
+      )}
     </ChatInputProvider>
   )
 }
